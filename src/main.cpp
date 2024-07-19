036b532b73ba565965d8d3012b1ede24deecfbbe
Bernu_LI <anufrievv852@gmail.com>
2024-07-19 19:00:18 +0300

    first triangle

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,40 @@
 
 #include <iostream>
 
+/* Array of vertex coordinates (already normalized) */
+GLfloat vertices[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f, 
+   -0.5f, -0.5f, 0.0f
+};
+
+/* Array of vertex colors */
+GLfloat colors[] = {
+    1.0f, 0.0f, 0.0f,
+    0.0f, 1.0f, 0.0f,
+    0.0f, 0.0f, 1.0f
+};
+
+/* Code for the vertex shader */
+const char* vertex_shader = 
+"#version 330\n"    // GLSL version
+"layout(location = 0) in vec3 vertex_position;"     // Declaration of input parameter
+"layout(location = 1) in vec3 vertex_color;"        // Declaration of input parameter
+"out vec3 color;"    // Declaration of output variable
+"void main() {"     // Function with shader logic
+"   color = vertex_color;"
+"   gl_Position = vec4(vertex_position, 1.0f);" // Definition of vertex position
+"}";
+
+/* Code for the fragment shader */
+const char* fragment_shader = 
+"#version 330\n"    // GLSL version
+"in vec3 color;"    // Take the value set in the vertex shader
+"out vec4 fragment_color;"    //    Declaration of output variable (defines the fragment color)  
+"void main() {"
+"   fragment_color = vec4(color, 1.0f);" 
+"}";
+
 /* Global variables for window size */
 int gWindowSizeX = 640;
 int gWindowSizeY = 480;
@@ -23,7 +57,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
 }
 
 int main(void)
-{
+{   
     /* Initialize the library */
     if (!glfwInit()) {
         std::cout << "glfwInit failed" << std::endl;
@@ -31,9 +65,9 @@ int main(void)
     }
 
     /* Set the minimum required version of OpenGL */
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
     /* Create a windowed mode window and its OpenGL context */
     GLFWwindow* pWindow = glfwCreateWindow(gWindowSizeX, gWindowSizeY, "OpenGL_Training ", nullptr, nullptr);
     if (!pWindow)
@@ -63,11 +97,63 @@ int main(void)
 
     glClearColor(0, 1, 0, 1);
 
+    /* Vertex shader initialization */
+    GLuint vs = glCreateShader(GL_VERTEX_SHADER);   // Create a vertex shader object and return its unique id
+    glShaderSource(vs, 1, &vertex_shader, nullptr);     // Bind source code to the shader object
+    glCompileShader(vs);    // Compile source code
+    
+    /* Fragment shader initialization */
+    GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);     // Create a fragment shader object and return its id
+    glShaderSource(fs, 1, &fragment_shader, nullptr);   // Bind source code to the shader object
+    glCompileShader(fs);    // Compile source code
+
+    /* Combine independent shader objects into a shader program */
+    GLuint shader_program = glCreateProgram();  // Create a program object and return its unique identifier
+    glAttachShader(shader_program, vs);     // Attach a vertex shader to the program object
+    glAttachShader(shader_program, fs);     // Attach a fragment shader to the program object 
+    glLinkProgram(shader_program);      // Link the program object
+    
+    /* Delete no longer needed shader objects */
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    /* Create a Vertex Buffer Object with vertex coordinate data in video card memory */
+    GLuint vertices_vbo = 0;    
+    glGenBuffers(1, &vertices_vbo);     // Create one buffer and return its unique identidier
+    glBindBuffer(GL_ARRAY_BUFFER, vertices_vbo); // Bind the buffer to GL_ARRAY_BUFFER type & make it current
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // Fill buffer with data
+
+    /* Create a Vertex Buffer Object with color data in video card memory */
+    GLuint colors_vbo = 0;
+    glGenBuffers(1, &colors_vbo);     // Create one buffer and return its unique identidier
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);  // Bind the buffer to GL_ARRAY_BUFFER type & make it current
+    glBufferData(GL_ARRAY_BUFFER, sizeof(colors), colors, GL_STATIC_DRAW);  // Fill buffer with data
+
+    /* Create a Vertex Array Object for vertex attribute state */
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);     // Create one vertex array and return its unique identifier
+    glBindVertexArray(vao);     // Make the vertex array current
+
+    /* Configure the vertex shader to work with coordinates */
+    glEnableVertexAttribArray(0);   // Enable use of vertex attribute with index 0 in the vertex shader
+    glBindBuffer(GL_ARRAY_BUFFER, vertices_vbo); // Bind the buffer to GL_ARRAY_BUFFER type & make it current
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr); // Configure how the driver interprets data
+
+    /* Configure the vertex shader to work with colors */
+    glEnableVertexAttribArray(1);   // Enable use of vertex attribute with index 1 in the vertex shader
+    glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);  // Bind the buffer to GL_ARRAY_BUFFER type & make it current
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr); // Configure how the driver interprets data
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Render an object */
+        glUseProgram(shader_program);   // Activate shader program (make it current)
+        glBindVertexArray(vao);     //  Make the vertex array current
+        glDrawArrays(GL_TRIANGLES, 0, 3);   // Render an object
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
